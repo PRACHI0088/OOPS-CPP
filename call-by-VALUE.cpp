51ec9dee1dde45a0f612c382972576a2f36b33a2
PRACHI <99760316+PRACHI0088@users.noreply.github.com>
2023-10-29 23:48:15 +0530

    Add files via upload

---
+++ call-by-VALUE.cpp
@@ -0,0 +1,20 @@
+// to swap two numbers using function call by value.
+#include<iostream>
+using namespace std;
+void swap(int , int );
+int main()
+{ int a,b;
+  cout<<"Enter the numbers to be swapped. \n";
+  cin>>a>>b;
+  cout<<"Numbers BEFORE Swapping are: \t"<<a<<"\t"<<b; 
+  cout<<"\n The SWAPPED numbers are:\t";
+  swap(a,b);
+  return 0; 
+}
+ void swap(int x, int y)
+   { int temp;
+     temp= x;
+     x= y;
+     y=temp;
+     cout<<x<<" "<<y;
+   }
